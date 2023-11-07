0bd236846258974f5fd1e4583c021fe1e327a949
savedim <savedimdev@gmail.com>
2023-11-07 17:14:45 +0300

    Add minor changes

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-import sqlite3
-
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
@@ -31,8 +29,21 @@ def about():
     return render_template('about.html')
 
 
-@app.route('/create-article')
+@app.route('/create-article', methods=['POST', 'GET'])
 def create_article():
+    if request.method == "POST":
+        title = request.form['title']
+        intro = request.form['intro']
+        text = request.form['text']
+
+        article = Article(title=title, intro=intro, text=text)
+
+        try:
+            db.session.add(article)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "При добвылении статьи произошла ошибка!"
     return render_template('create-article.html')
 
 @app.route('/user/<string:name>/<int:id>')
