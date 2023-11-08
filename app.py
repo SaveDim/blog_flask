fb6d83d94a734d825ff856d6f7d8449492f2e922
savedim <savedimdev@gmail.com>
2023-11-08 11:20:46 +0300

    Add posts page

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
 db = SQLAlchemy(app)
@@ -17,6 +18,30 @@ class Article(db.Model):
         return '<Article %r>' % self.id
 
 
+class Tasks(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    task_title = db.Column(db.String(50), nullable=False)
+    task_text = db.Column(db.Text, nullable=False)
+    answer = db.Column(db.Text, nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow())
+    difficulty = db.Column(db.Float, default=1.0)
+
+    def __repr__(self):
+        return '<Tasks %r>' % self.id % self.task_title
+
+
+class Someinfo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), nullable=False)
+    phone_number = db.Column(db.String(15), nullable=False)
+    name = db.Column(db.String(20), nullable=False)
+    age = db.Column(db.Integer)
+    happyness_level = db.Column(db.Integer, nullable=False)
+    has_car = db.Column(db.Boolean, default=False)
+
+    def __repr__(self):
+        return '<Someinfo %r>' % self.id
+
 
 @app.route('/')
 @app.route('/home')
@@ -28,6 +53,11 @@ def index():
 def about():
     return render_template('about.html')
 
+
+@app.route('/posts')
+def posts():
+    articles = Article.query.order_by(Article.date).all()
+    return render_template('posts.html', articles=articles)
 
 @app.route('/create-article', methods=['POST', 'GET'])
 def create_article():
@@ -43,8 +73,9 @@ def create_article():
             db.session.commit()
             return redirect('/')
         except:
-            return "При добвылении статьи произошла ошибка!"
-    return render_template('create-article.html')
+            return "При добавлении статьи произошла ошибка!"
+    else:
+        return render_template('create-article.html')
 
 @app.route('/user/<string:name>/<int:id>')
 def user(name, id):
@@ -75,4 +106,6 @@ def contacts():
     return render_template('contacts.html')
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
